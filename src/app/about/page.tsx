982e84e95203956ff7e409fa1098fe785a8b6581
emdecr <10912003+emdecr@users.noreply.github.com>
2025-03-15 16:27:35 -0400

    Update config and add new routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+    title: 'About - Emily Dela Cruz',
+    description: '',
+    openGraph: {
+        title: 'About - Emily Dela Cruz',
+        description: '',
+        url: '',
+        image: '',
+    },
+    twitter: {
+        title: 'About - Emily Dela Cruz',
+        description: '',
+        image: '',
+    },
+};
+
+export default function About() {
+    return (
+        <div>
+            <h1>Welcome to /about</h1>
+        </div>
+    );
+}
---
+++ src/app/records/page.tsx
@@ -0,0 +1,76 @@
+"use client"; // This is a client-side component
+
+// Import React hooks and Image component
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+// Define Article interface
+export interface Article {
+  id: string;
+  title: string;
+  content: string;
+  cover: any;
+  publishedAt: Date;
+}
+
+// Define Strapi URL
+const STRAPI_URL = "http://localhost:1337";
+
+export default function Records() {
+  // Define articles state
+  const [articles, setArticles] = useState<Article[]>([]);
+
+  // Fetch articles
+  const getArticles = async () => {
+    const response = await fetch(`${STRAPI_URL}/api/articles?populate=*`);
+    const data = await response.json();
+    setArticles(data.data);
+  };
+
+  // Format date
+  const formatDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+
+  // Fetch articles on component mount
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-8">Articles</h1>
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {articles.map((article) => (
+            <article
+              key={article.id}
+              className="bg-white shadow-md rounded-lg overflow-hidden"
+            >
+              <Image
+                className="w-full h-48 object-cover"
+                src={STRAPI_URL + article.cover.url}
+                alt={article.title}
+                width={180}
+                height={38}
+                priority
+              />
+              <div className="p-4">
+                <h3 className="text-lg font-bold mb-2">{article.title}</h3>
+                <p className="text-gray-600 mb-4">{article.content}</p>
+                <p className="text-sm text-gray-500">
+                  Published: {formatDate(article.publishedAt)}
+                </p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
